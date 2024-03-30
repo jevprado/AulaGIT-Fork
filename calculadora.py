92476ceb9396def31d5342a18b2534bc0a69c992
jevprado <jevrprado@gmail.com>
2024-03-30 17:24:29 -0300

    configurando calculadora de media de ted1

--- calculadora.py
+++ calculadora.py
@@ -1,14 +1,11 @@
-# calculadora python de notas -> C1 - UnB - Professor Fragelli 
+# calculadora python de notas -> Teoria de eletronica digital 1 - Professor Renato
 
-nota1 = float(input("Qual a sua nota de questionarios: "))
-nota2 = float(input("Qual a sua nota da P1: "))
-nota3 = float(input("Qual a sua nota da P2: "))
-nota4 = float(input("Qual a sua nota da P3: "))
+nota1 = float(input("Qual a sua nota da P1: "))
+nota2 = float(input("Qual a sua nota da P2: "))
 
-notas = nota1*1 + nota2*2 + nota3*2 + nota4*3 
-media = notas/8 
+notas = nota1*0.4 + nota2*0.6
 
-if media > 5.0: 
-    print(f"A sua nota na disciplina de Cálculo 1 foi {media}. Parabéns, voce esta aprovado!")
+if notas > 5.0: 
+    print(f"A sua nota na disciplina de Cálculo 1 foi {notas}. Parabéns, voce esta aprovado!")
 else: 
-    print(f"A sua nota na disciplina de Cálculo 1 foi {media}. Infelizmente, voce esta reprovado!")
+    print(f"A sua nota na disciplina de Cálculo 1 foi {notas}. Infelizmente, voce esta reprovado!")

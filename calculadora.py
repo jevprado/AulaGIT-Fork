5b9a1d14f593bfda91c82014410e12e74ef3f23a
jevprado <jevrprado@gmail.com>
2024-03-30 17:26:38 -0300

    ajustando a mensagem final

--- calculadora.py
+++ calculadora.py
@@ -6,6 +6,6 @@ nota2 = float(input("Qual a sua nota da P2: "))
 notas = nota1*0.4 + nota2*0.6
 
 if notas > 5.0: 
-    print(f"A sua nota na disciplina de Cálculo 1 foi {notas}. Parabéns, voce esta aprovado!")
+    print(f"A sua nota na disciplina de TED 1 foi {notas}. Parabéns, voce esta aprovado!")
 else: 
-    print(f"A sua nota na disciplina de Cálculo 1 foi {notas}. Infelizmente, voce esta reprovado!")
+    print(f"A sua nota na disciplina de TED 1 foi {notas}. Infelizmente, voce esta reprovado!")
